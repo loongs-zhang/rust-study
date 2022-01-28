44c859dc91cc487d3f357da0662e037c20a78137
hailang <hailang@webuy.ai>
2022-01-28 23:23:09 +0800

    move code

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,7 @@
-use std::collections::LinkedList;
 use std::ops::RangeInclusive;
-use std::time::{SystemTime, UNIX_EPOCH};
 include!("request.rs");
 
-#[tokio::main]
-async fn main() {
+fn main() {
     println!("Hello, world!");
-    let start = current_time_millis();
-    tokio::join!(request("1".to_string()),
-    request("2".to_string()),
-    request("3".to_string()),
-    request("4".to_string()),
-    request("5".to_string()),
-    request("6".to_string()),
-    request("7".to_string()),
-    request("8".to_string()),
-    request("9".to_string()),
-    request("10".to_string()),
-    request("11".to_string()),
-    request("12".to_string()),
-    request("13".to_string()),
-    request("14".to_string()),
-    request("15".to_string()),
-    request("16".to_string()),);
-    println!("cost:{:?}ms", current_time_millis() - start);
-}
-
-fn current_time_millis() -> i64 {
-    let since_the_epoch = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    let ms = since_the_epoch.as_secs() as i64 * 1000i64 + (since_the_epoch.subsec_nanos() as f64 / 1_000_000.0) as i64;
-    return ms;
+    test();
 }
--- src/request.rs
+++ src/request.rs
@@ -1,4 +1,27 @@
 use std::collections::HashMap;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[tokio::main]
+pub async fn test() {
+    let start = current_time_millis();
+    tokio::join!(request("1".to_string()),
+    request("2".to_string()),
+    request("3".to_string()),
+    request("4".to_string()),
+    request("5".to_string()),
+    request("6".to_string()),
+    request("7".to_string()),
+    request("8".to_string()),
+    request("9".to_string()),
+    request("10".to_string()),
+    request("11".to_string()),
+    request("12".to_string()),
+    request("13".to_string()),
+    request("14".to_string()),
+    request("15".to_string()),
+    request("16".to_string()),);
+    println!("cost:{:?}ms", current_time_millis() - start);
+}
 
 pub async fn request(id: String) -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("https://httpbin.org/ip")
@@ -7,4 +30,12 @@ pub async fn request(id: String) -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     println!("{}\n{:#?}", id, resp);
     Ok(())
+}
+
+pub fn current_time_millis() -> i64 {
+    let since_the_epoch = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let ms = since_the_epoch.as_secs() as i64 * 1000i64 + (since_the_epoch.subsec_nanos() as f64 / 1_000_000.0) as i64;
+    return ms;
 }
