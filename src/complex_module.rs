4b00933fa04cc9ba1600ce1e1f99f1dc2c93e73d
hailang <hailang@webuy.ai>
2022-02-19 23:25:52 +0800

    pub use

--- src/complex_module.rs
+++ src/complex_module.rs
@@ -1,3 +1,8 @@
 pub mod module1;
+//重新导出，对用户友好
+//原来的写法use rust-study::module1::hello;
+//导出后的写法use rust-study::hello;
+pub use self::module1::hello;
+pub use self::module1::world;
 
 pub mod module2;
