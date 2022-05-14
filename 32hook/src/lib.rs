72fe03ca650a6af48207bd1c2421c35b700feccc
hailang <hailang@webuy.ai>
2022-05-14 16:11:41 +0800

    成功调用原始系统函数

--- 32hook/src/lib.rs
+++ 32hook/src/lib.rs
@@ -1,6 +1,14 @@
+use core::mem;
+use std::os::raw::c_uint;
+
 //被hook的系统函数
 #[no_mangle]
-pub extern "C" fn sleep(i: usize) -> usize {
-    println!("hooked {}", i);
-    i
+pub extern "C" fn sleep(i: c_uint) -> c_uint {
+    println!("hooked, param:{}", i);
+    //获取原始系统函数
+    let original = unsafe {
+        mem::transmute::<_, extern "C" fn(c_uint) -> c_uint>(libc::dlsym(libc::RTLD_NEXT, "sleep".as_ptr() as _))
+    };
+    //调用原始系统函数
+    original(i)
 }
