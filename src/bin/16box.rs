acbaadc5b284eeccc133f594dcfb948acda31416
hailang <hailang@webuy.ai>
2022-06-05 22:46:30 +0800

    add box example

--- src/bin/16box.rs
+++ src/bin/16box.rs
@@ -1,3 +1,5 @@
+use std::alloc::{alloc, Layout};
+
 /**
 Box<T>适用场景：
 1.在编译时，无法确定类型的大小，但使用这个类型的值时，上下文又需要知道它的大小；
@@ -15,4 +17,12 @@ fn main() {
     //Box<T>会把数据存储在heap上，stack上只留下指针
     let boxed = Box::new(5);
     println!("{}", boxed);
+    let test = || {
+        println!("test");
+    };
+    let text = Box::new(test);
+    let ptr = Box::into_raw(text);
+    unsafe {
+        (*ptr)();
+    }
 }//在代码运行到这里时，boxed对应的stack里指针和heap里的数据都会被清理
