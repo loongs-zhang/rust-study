a542fc8d47dea3c56ebc1e5f03a62cc9f835f72a
hailang <a1936978077@hotmail.com>
2022-10-25 00:19:41 +0800

    add signal example

---
+++ src/bin/42signal.rs
@@ -0,0 +1,25 @@
+use std::os::unix::thread::JoinHandleExt;
+use std::thread;
+use std::time::Duration;
+
+static mut RUN: bool = true;
+
+unsafe fn test() {
+    println!("change flag");
+    RUN = false;
+}
+
+fn main() -> thread::Result<()> {
+    let pthread = thread::spawn(|| unsafe {
+        println!("register signal");
+        libc::signal(libc::SIGURG, test as usize);
+        println!("start loop");
+        while RUN {}
+        println!("finished");
+    }).as_pthread_t();
+    thread::sleep(Duration::from_millis(1000));
+    thread::spawn(move || unsafe {
+        libc::pthread_kill(pthread, libc::SIGURG);
+        println!("pthread signaled");
+    }).join()
+}
